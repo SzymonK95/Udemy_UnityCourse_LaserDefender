4f4c6b0f1f4b07c9e7128a8457e19a12988adb84
SzymonK95 <szymon_kaszuba97@wp.pl>
2018-05-13 09:25:31 +0200

    Finish section 6 lecture 105

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("New Level load: " + name);
+        //Brick.BreakableCount = 0;
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        //Brick.BreakableCount = 0;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void BrickDestroyed()
+    {
+        if (true)//Brick.BreakableCount <= 0)
+        {
+            LoadNextLevel();
+        }
+    }
+
+}
---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour
+{
+
+    public LevelManager LevelManager;
+
+    void OnTriggerEnter2D (Collider2D trigger)
+    {
+        LevelManager = GameObject.FindObjectOfType<LevelManager>();
+        LevelManager.LoadLevel("Lose Screen");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
+}
